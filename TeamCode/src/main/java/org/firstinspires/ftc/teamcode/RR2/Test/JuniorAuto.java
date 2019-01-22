094cc2a9e3fb603ba6c30cff7e3aeecec8380e74
albertcai101 <albertcai.paloalto@gmail.com>
2019-01-21 20:57:24 -0800

    junior

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2/Test/JuniorAuto.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.RR2.Test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@Autonomous(name="JuniorAuto", group="Linear Opmode")
+public class JuniorAuto extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        leftDrive.setPower(1);
+        rightDrive.setPower(1);
+        sleep(1000);
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        leftDrive.setPower(-1);
+        rightDrive.setPower(-1);
+        sleep(1000);
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+    }
+}
+
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2/Test/JuniorTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR2/Test/JuniorTeleOp.java
@@ -51,9 +51,9 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+@TeleOp(name="JuniorTeleOp", group="Linear Opmode")
 //@Disabled
-public class DummyRobot extends LinearOpMode {
+public class JuniorTeleOp extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
